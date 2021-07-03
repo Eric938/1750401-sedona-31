d33643070220e454dd2941e435820fb575f5a3fe
Eric938 <89991891544@yandex.ru>
2021-07-03 19:16:16 +0400

    исправления

--- js/js.js
+++ js/js.js
@@ -1,14 +1,14 @@
-const findButton = document.querySelector('.find-button');
-const popup = document.querySelector('.popup-form-wrapper');
-const popupForm = popup.querySelector('.popup-form');
-const dateArrival = popup.querySelector('.date-arrival');
-const dateDeparture = popup.querySelector('.date-departure');
-const adults = popup.querySelector('.adults');
-const kids = popup.querySelector('.kids');
+const findButton = document.querySelector(".find-button");
+const popup = document.querySelector(".popup-form-wrapper");
+const popupForm = popup.querySelector(".popup-form");
+const dateArrival = popup.querySelector(".date-arrival");
+const dateDeparture = popup.querySelector(".date-departure");
+const adults = popup.querySelector(".adults");
+const kids = popup.querySelector(".kids");
 
 findButton.addEventListener("click",function (evt) {
   evt.preventDefault();
-  popup.classList.toggle('popup-show');
+  popup.classList.toggle("popup-show");
 });
 
 popupForm.addEventListener("submit", function (evt) {
